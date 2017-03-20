85cb3c61ec5e509720418ea7918ff2575609d715
Aidan Lewis <alewis9@nd.edu>
2017-03-20 11:17:02 -0400

    main.cpp: start

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Jessica Hardey, Bart Janczuk, Josefa Osorio, Aidan Lewis
+// main.cpp
+
+#include "cell.h"
+#include "maze.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  maze test;
+}
