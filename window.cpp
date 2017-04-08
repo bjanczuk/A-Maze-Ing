9714a8dff42617ca1719c91bdc2ec4af09b9f160
Josefa Joyce Osorio <josorio2@cushing41.helios.nd.edu>
2017-04-08 17:01:50 -0400

    window.cpp: updated

--- window.cpp
+++ window.cpp
@@ -11,14 +11,26 @@ int main(int argc, char *argv[])
 {
 	SDL_Window *window = NULL;
 	SDL_Surface *screenSurface = NULL;
+	bool quit = false;
+	SDL_Event event;
 	
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 		cout << SDL_GetError() << endl;
-	window = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow("Maze Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	
 	if (window == NULL)
 		cout << "Window failed to open" << endl;
+
 	screenSurface = SDL_GetWindowSurface(window);
-	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_RenderDrawLine(renderer, 10,10,10,10);
+
+
+	//SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 	SDL_UpdateWindowSurface(window);
 	SDL_Delay(3000);
+	*/
+
+	
+
 }
