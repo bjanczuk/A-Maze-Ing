a214592b66fdddeb324e0736f72722c530d99885
Josefa Joyce Osorio <josorio2@cushing54.helios.nd.edu>
2017-04-18 14:40:54 -0400

    window.cpp updated: lines added

--- window.cpp
+++ window.cpp
@@ -9,6 +9,7 @@ const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char *argv[])
 {
+	/*
 	SDL_Window *window = NULL;
 	SDL_Surface *screenSurface = NULL;
 	bool quit = false;
@@ -23,14 +24,48 @@ int main(int argc, char *argv[])
 
 	screenSurface = SDL_GetWindowSurface(window);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_RenderDrawLine(renderer, 10,10,10,10);
+	//SDL_RenderDrawLine(renderer, 10,10,10,10);
 
 
-	//SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 	SDL_UpdateWindowSurface(window);
 	SDL_Delay(3000);
-	
+	*/
 
-	
+	if(SDL_Init(SDL_INIT_VIDEO) == 0){
+		SDL_Window* window = NULL;
+		SDL_Renderer* renderer = NULL;
 
+		if(SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0){
+			SDL_bool done = SDL_FALSE;
+
+			while(!done){
+				SDL_Event event;
+				SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+				
+				//format: SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+				SDL_RenderDrawLine(renderer, 200, 200, 200, 300);
+				SDL_RenderDrawLine(renderer, 300, 200, 300, 300);
+				SDL_RenderDrawLine(renderer, 200, 250, 300, 250);
+				SDL_RenderDrawLine(renderer, 350, 250, 350, 300);
+				SDL_RenderDrawLine(renderer, 350, 225, 350, 226);
+				SDL_RenderPresent(renderer);
+
+				while(SDL_PollEvent(&event)){
+					if(event.type == SDL_QUIT){
+						done = SDL_TRUE;
+					}
+				}
+			}
+		}
+		
+		if(renderer){
+			SDL_DestroyRenderer(renderer);
+		}
+		if(window){
+			SDL_DestroyWindow(window);
+		}
+	}
+	SDL_Quit();
+	return 0;
 }
